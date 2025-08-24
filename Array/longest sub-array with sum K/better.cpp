845970d8dcf22fc636d8058cf824f92fb2f7e198
4rayaditya <rayaditya731@gmail.com>
2025-08-24 23:24:44 +0530

    "Day 04"

---
+++ Array/longest sub-array with sum K/better.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int> &a, int k)
+{
+    int n = a.size();
+
+    int len = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int s = 0;
+        for (int j = i; j < n; j++)
+        {
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+int main()
+{
+    vector<int> a = {-1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+
+// time complexity = O(N^2)
+// space complexity = O(1)
